dc9954ff5dedd541fd2372803f1574155c404cdc
Chandan Rabha <chandan@wecreate.app>
2021-12-20 17:10:53 +0000

    Merged PR 2688: Unwanted import

--- components/educationPage/bannerSection/bannerSection.tsx
+++ components/educationPage/bannerSection/bannerSection.tsx
@@ -1,4 +1,3 @@
-import { Button } from "react-bootstrap";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
