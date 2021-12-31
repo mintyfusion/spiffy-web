ab318292265ce58cb34778f1788c4d89220b817b
Usama <58435075+osama554@users.noreply.github.com>
2021-12-31 17:21:41 +0500

    changes by merge

--- components/educationDetailsPage/detailsSection/detailsSection.module.scss.d.ts
+++ components/educationDetailsPage/detailsSection/detailsSection.module.scss.d.ts
@@ -9,6 +9,9 @@ export type Styles = {
   "bold60": string;
   "bold67": string;
   "bold90": string;
+  "boldDesktop41Mobile25": string;
+  "boldGame41": string;
+  "boldGame67": string;
   "detailsContainer": string;
   "extraBold67": string;
   "extraBold90": string;
@@ -16,13 +19,17 @@ export type Styles = {
   "fullbold109": string;
   "fullBold67": string;
   "mediaContainer": string;
+  "medium25": string;
+  "regular14": string;
   "regular16": string;
+  "regular20": string;
   "regular24": string;
   "regular25": string;
   "regular32": string;
   "regular90": string;
   "regularDesktop25Mobile16": string;
   "regularDesktop25Mobile9": string;
+  "regularDesktop30Mobile20": string;
   "semiBold90": string;
 };
 
