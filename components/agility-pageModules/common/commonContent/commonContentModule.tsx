574cefa4b600ce89bab33eb2a46fbffc95ad65b5
Chandan Rabha <changrabha.rabha1@gmail.com>
2022-02-03 17:00:47 +0530

    removing unused files

--- components/agility-pageModules/common/commonContent/commonContentModule.tsx
+++ components/agility-pageModules/common/commonContent/commonContentModule.tsx
@@ -1,13 +1,13 @@
 import { ModuleProps } from "@agility/nextjs";
 import React from "react";
 
-import ICommonContentProps from "components/agility-pageModules/common/commonContent/interfaces/ICommonContentProps";
+import ICommonContent from "components/agility-pageModules/common/commonContent/interfaces/ICommonContent";
 import Section from "components/agility-pageModules/common/section/section";
 import SectionSide from "components/agility-pageModules/common/section/enums/SectionSide";
 
 import styles from "components/agility-pageModules/common/commonContent/commonContentmodule.module.scss";
 
-const CommonContentModule = (props: ModuleProps<ICommonContentProps>): JSX.Element => {
+const CommonContentModule = (props: ModuleProps<ICommonContent>): JSX.Element => {
     const { title, description, invertedColors, href } = props.module.fields;
 
     return (
--- components/agility-pageModules/common/commonContent/interfaces/ICommonContentProps.ts
+++
@@ -1,5 +0,0 @@
-import ICommonContent from "components/agility-pageModules/common/commonContent/interfaces/ICommonContent";
-
-export default interface ICommonContentProps extends ICommonContent {
-    page: { name: string };
-}
