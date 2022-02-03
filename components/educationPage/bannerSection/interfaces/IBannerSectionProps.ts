a4b3d611e05809be9aa4a53dc1ca06af4c67217f
Chandan Rabha <changrabha.rabha1@gmail.com>
2022-02-03 14:53:28 +0530

    removed unused files

--- components/educationPage/bannerSection/interfaces/IBannerSectionProps.ts
+++
@@ -1,5 +0,0 @@
-import IContentInfo from "types/IContentnfo";
-
-export default interface IBannerSectionProps extends IContentInfo {
-    renderSearch?: boolean;
-}
--- components/educationPage/bannerSection/interfaces/ISearchInfo.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default interface ISearchInfo {
-    searchValue: string;
-    setSearch: React.Dispatch<React.SetStateAction<string>>;
-}
