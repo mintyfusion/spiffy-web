5a629c6af2963059d8deacf65d6a78171a3123d2
Chandan Rabha <changrabha.rabha1@gmail.com>
2022-02-11 19:05:52 +0530

    proper api key based on node environment

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -3,10 +3,21 @@ import agility from "@agility/content-fetch";
 import IMainApi from "utils/api/interfaces/IMainApi";
 
 const mainAPI = agility as IMainApi;
+const env = process.env.NODE_ENV as string;
+
+const apiParams = env === "development"
+    ? {
+        apiKey: process.env.NEXT_PUBLIC_AGILITY_API_PREVIEW_KEY,
+        isPreview: true
+    }
+    : {
+        apiKey: process.env.NEXT_PUBLIC_AGILITY_API_FETCH_KEY,
+        isPreview: false
+    };
+
 const api = mainAPI.getApi({
     guid: process.env.NEXT_PUBLIC_AGILITY_GUID,
-    apiKey: process.env.NEXT_PUBLIC_AGILITY_API_PREVIEW_KEY,
-    isPreview: true
+    ...apiParams
 });
 
 export default api;
