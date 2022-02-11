cc625a5784e9f31a8475c7f96a340851419eb044
Chandan Rabha <changrabha.rabha1@gmail.com>
2022-02-11 18:42:13 +0530

    pr fixes

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import agility from "@agility/content-fetch";
 
-import IApi from "utils/api/interfaces/IApi";
+import IMainApi from "utils/api/interfaces/IMainApi";
 
-const api: IApi = agility.getApi({
+const mainAPI = agility as IMainApi;
+const api = mainAPI.getApi({
     guid: process.env.NEXT_PUBLIC_AGILITY_GUID,
     apiKey: process.env.NEXT_PUBLIC_AGILITY_API_PREVIEW_KEY,
     isPreview: true
--- utils/api/interfaces/IApiData.ts
+++ utils/api/interfaces/IApiData.ts
@@ -1,6 +1,6 @@
 import IContentList from "utils/api/interfaces/IContentList";
 import IGetContentList from "utils/api/interfaces/IGetContentList";
 
-export default interface IApi {
+export default interface IApiData {
     getContentList<T>(props: IGetContentList): Promise<IContentList<T>>;
 }
