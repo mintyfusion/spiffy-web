e8d69c4e699e354ba8aa16c06f39814aa3aeceb6
Chandan Rabha <changrabha.rabha1@gmail.com>
2022-02-14 17:44:36 +0530

    pr fix

--- components/agility-pageTemplates/index.tsx
+++ components/agility-pageTemplates/index.tsx
@@ -2,23 +2,12 @@ import { ComponentWithInit } from "@agility/nextjs";
 
 import MainTemplate from "components/agility-pageTemplates/MainTemplate";
 
-interface ITemplates {
-    name: string;
-    template: ComponentWithInit;
-}
-
 // All of the Agility Page Template Components that are in use in this site need to be imported into this index file.
 // Place Page Templates in allTemplates array below, passing in a name and the component.
-const allTemplates: ITemplates[] = [
-    { name: "MainTemplate", template: MainTemplate },
-];
+const allTemplates: Record<string, ComponentWithInit> = { MainTemplate };
 
 export const getPageTemplate = (templateName: string): ComponentWithInit => {
     if (!templateName) return null;
-    const obj = allTemplates.find(
-        (m) => m.name.toLowerCase() === templateName.toLowerCase()
-    );
-    if (!obj) return null;
 
-    return obj?.template;
+    return allTemplates[templateName];
 };
