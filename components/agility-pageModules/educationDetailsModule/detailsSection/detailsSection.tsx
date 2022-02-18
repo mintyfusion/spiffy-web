5eadd4dd785af6ec3e29ffdb97fd9a3f7271287f
Chandan Rabha <hello@mintyfusion.com>
2022-02-18 14:09:51 +0530

    design fixes

--- components/agility-pageModules/educationDetailsModule/detailsSection/detailsSection.tsx
+++ components/agility-pageModules/educationDetailsModule/detailsSection/detailsSection.tsx
@@ -2,17 +2,23 @@ import dynamic from "next/dynamic";
 import Image from "next/image";
 import React from "react";
 
+import BreakpointChecks from "hooks/enums/breakpointChecks";
+import Breakpoints from "common/style/breakpoints";
 import DetailsType from "components/agility-pageModules/educationDetailsModule/detailsSection/enums/detailsType";
 import IDetailsSectionProps from "components/agility-pageModules/educationDetailsModule/detailsSection/interfaces/IDetailsSectionProps";
 import renderHtml from "utils/renderHtml";
+import useBreakpoint from "hooks/useBreakpoint";
 
 import styles from "components/agility-pageModules/educationDetailsModule/detailsSection/detailsSection.module.scss";
 
 const VideoPlayer = dynamic(() => import("components/agility-pageModules/common/videoPlayer/videoPlayer"));
 
-const DetailsSection = (props: IDetailsSectionProps): JSX.Element => 
+const DetailsSection = (props: IDetailsSectionProps): JSX.Element => {
+    const breakpoint = useBreakpoint(Breakpoints.LG, BreakpointChecks.Greater);
+
+    return (
         <div className={`${styles.detailsContainer} position-relative`}>
-            <h5 className="text-capitalize">{props.title}</h5>
+            {breakpoint && <h5 className="text-capitalize">{props.title}</h5>}
             <h1>{props.description}</h1>
             <div className={styles.mediaContainer}>
                 {props.detailsType == DetailsType.guide
@@ -27,6 +33,8 @@ const DetailsSection = (props: IDetailsSectionProps): JSX.Element =>
                 }
             </div>
             <div dangerouslySetInnerHTML={renderHtml(props.htmlContent)} />
-        </div>;
+        </div>
+    );
+};
 
 export default DetailsSection;
