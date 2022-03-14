8d4fc8a7688b3d020e83342b6fe937ce882b152a
Usama <hello@mintyfusion.com>
2022-03-14 10:49:25 +0500

    signup section button href

--- components/agility-pageModules/game/gameSignup/gameSignup.tsx
+++ components/agility-pageModules/game/gameSignup/gameSignup.tsx
@@ -5,14 +5,18 @@ import flexbox from "utils/flexbox";
 import PrimaryButton from "components/agility-pageModules/common/primaryButton/primaryButton";
 
 import styles from "components/agility-pageModules/gameSignup/gameSignup.module.scss";
+import ConstantUtils from "utils/constantUtils";
 
 const colCenter = flexbox({ vertical: true, hAlign: "center", vAlign: "center" });
+const linkProps = {
+    href: ConstantUtils.dashboardPath
+};
 
 const GameSignup = (): JSX.Element => <div className={`${styles.gameStepSix} w-100`}>
     <div className={`${colCenter} ${styles.signUpsection}`}>
         <Image src="/images/game/trophy.png" alt="trophy" width={291} height={318} />
         <h2 className={`${styles.avatarHeading}`}>Congratulations!</h2>
-        <PrimaryButton className="w-100" linkProps={{ href: "https://creator.dashboard.spiffy.biz/" }}>
+        <PrimaryButton className="w-100" linkProps={linkProps}>
             Sign Up
         </PrimaryButton>
     </div>
