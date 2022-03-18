b1f399b7f0b18f1098d39fff10a1d926813036cd
Usama <hello@mintyfusion.com>
2022-03-18 12:27:01 +0500

    banner height

--- components/agility-pageModules/common/banner/banner.tsx
+++ components/agility-pageModules/common/banner/banner.tsx
@@ -3,26 +3,34 @@ import Image, { ImageProps } from "next/image";
 import React, { PropsWithChildren } from "react";
 
 import IBannerProps from "components/agility-pageModules/common/banner/interfaces/IBannerProps";
+import setViewportHeight from "utils/setViewportHeight";
 
 import styles from "components/agility-pageModules/common/banner/banner.module.scss";
 
-const Banner = (props: PropsWithChildren<IBannerProps>): JSX.Element =>
-    <div className={`position-relative ${styles.bannerContainer}`}>
+const Banner = (props: PropsWithChildren<IBannerProps>): JSX.Element => {
+    React.useEffect(() => {
+        setViewportHeight();
+    }, []);
+
+    return <div className={`position-relative ${styles.bannerContainer}`}>
         {props.children}
-        <Carousel controls={false} indicators={false} pause={false} variant="dark" className="position-relative">
-            {props.images.map((banner: ImageProps, index) =>
-                <Carousel.Item
-                    className={`
-                    ${styles.carouselItem} 
-                    ${props.fullHeight ? styles.full : styles.half}
-                    position-relative`}
-                    key={index}
-                >
-                    <Image objectFit="cover" {...banner} width="1366" height="500" layout="intrinsic" />
-                </Carousel.Item>
-            )}
-        </Carousel>
-    </div>;
+        <Carousel controls={false} indicators={false} pause={false} variant="dark" className="position-relative" >
+            {
+                props.images.map((banner: ImageProps, index) =>
+                    <Carousel.Item
+                        className={`
+                        ${styles.carouselItem} 
+                        ${props.fullHeight ? styles.full : styles.half}
+                        position-relative`}
+                        key={index}
+                    >
+                        <Image objectFit="cover" {...banner} width="1366" height="500" layout="intrinsic" />
+                    </Carousel.Item>
+                )
+            }
+        </Carousel >
+    </div>
+}
 
 Banner.defaultProps = {
     fullHeight: true
