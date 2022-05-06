ede58b342bd2e007ffd8328bdc59d4c7ce97f25e
Usama <58435075+osama554@users.noreply.github.com>
2022-05-06 12:07:19 +0500

    import fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TagManager, { TagManagerArgs } from "react-gtm-module";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "styles/globals.scss";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 
 // Note: Putting this import at last to give more precedence to the components css
 //       instead of globals.scss and bootstrap.min.css
