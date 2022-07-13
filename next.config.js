8290377053f24666b7683ac1cde8b6766090cd49
Usama <usama@mintyfusion.com>
2022-07-13 12:13:44 +0500

    links fixed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+    reactStrictMode: true,
+    env: {
+        NEXT_AGILITY_GLASSHOUSE_URL: process.env.NEXT_AGILITY_GLASSHOUSE_URL,
+        NEXT_AGILITY_GLASSHOUSE_CREATOR_URL: process.env.NEXT_AGILITY_GLASSHOUSE_CREATOR_URL,
+    },
     images: {
         domains: ["cdn.aglty.io"],
+        loader: 'akamai',
     }
 }
